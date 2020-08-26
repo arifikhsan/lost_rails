a6e0f38e951622502ce9bcd7d219bc8073dd3dd9
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-26 11:20:57 +0700

    user detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_041329) do
+ActiveRecord::Schema.define(version: 2020_08_26_041544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_details", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "address_formatted_address"
+    t.string "address_street_number"
+    t.string "address_street_name"
+    t.string "address_street"
+    t.string "address_city"
+    t.string "address_zip_code"
+    t.string "address_department"
+    t.string "address_department_code"
+    t.string "address_state"
+    t.string "address_state_code"
+    t.string "address_country"
+    t.string "address_country_code"
+    t.float "address_lat"
+    t.float "address_lng"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_details_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_08_26_041329) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "user_details", "users"
 end
