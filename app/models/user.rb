86b69f424b1d3a2fce85fb9254ed8b9e6103650c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-26 11:26:38 +0700

    auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  has_one :user_detail
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:admin, :user]
+  after_initialize :set_default_role, :if => :new_record?
+
+  validates :email, :password, presence: true
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def self.admin
+    User.find_by(role: :admin)
+  end
 end
